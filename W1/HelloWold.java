8ef6566d8ecdbfb8d064893b3b37cbad8d6a8009
Hoàng Việt <83023633+MaG003@users.noreply.github.com>
2022-10-25 16:48:14 +0700

    Add files via upload

---
+++ W1/HelloWold.java
@@ -0,0 +1,8 @@
+package W1;
+
+public class HelloWold {
+    public static void main(String[] args){
+        System.out.println("Xin chao \nCac ban");
+        System.out.println("Hello World");
+    }
+}
