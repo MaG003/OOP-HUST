4ca976fa2891d747eb038d5102d18ee4417e3e98
Hoàng Việt <83023633+MaG003@users.noreply.github.com>
2022-10-26 21:38:00 +0700

    Delete HelloWold.java

--- W1/HelloWold.java
+++
@@ -1,8 +0,0 @@
-package W1;
-
-public class HelloWold {
-    public static void main(String[] args){
-        System.out.println("Xin chao \nCac ban");
-        System.out.println("Hello World");
-    }
-}
