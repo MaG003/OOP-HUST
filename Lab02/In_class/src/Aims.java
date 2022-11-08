5c7c483ea41c92ec2e00960f7fde1cabc81986ea
Hoàng Việt <83023633+MaG003@users.noreply.github.com>
2022-11-08 17:54:04 +0700

    Add files via upload

---
+++ Lab02/In_class/src/Aims.java
@@ -0,0 +1,9 @@
+public class Aims {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Carts anOrder = new Carts();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+    }
+}
---
+++ Lab02/In_class/src/Carts.java
@@ -0,0 +1,4 @@
+public class Carts {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+}
