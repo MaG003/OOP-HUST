07ff4740366f5e0f4b2ea39a6fd6e848d70562ba
Hoàng Việt <83023633+MaG003@users.noreply.github.com>
2022-12-06 17:15:20 +0700

    Add files via upload

---
+++ In_class/src/hust/soict/dsai/aims/store/Store.java
@@ -0,0 +1,22 @@
+package hust.soict.dsai.aims.store;
+
+import java.util.ArrayList;
+
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+
+public class Store {
+
+	ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+
+	public void removeDVD(DigitalVideoDisc disc) {
+		itemsInStore.remove(disc);
+		System.out.println("The disc " + disc.getTitle() + " has been removed");
+	}
+
+	public void addDVD(DigitalVideoDisc disc) {
+
+		itemsInStore.add(disc);
+		System.out.println("The disc " + disc.getTitle() + " has been added");
+	}
+
+}
